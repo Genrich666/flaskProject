95584c4a868c5a396b56a3b4b815af176a18c909
Genrich666 <vovangerok@gmail.com>
2025-01-08 21:46:58 +0300

    Add 4 website files

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-reviews = []  # Список для хранения отзывов (в реальной ситуации здесь будет база данных)
+reviews = []
+
+@app.route('/')
+def home():
+    return render_template('index.html')  # Путь к вашему HTML файлу
 
 @app.route('/api/reviews', methods=['POST'])
 def post_review():
-    data = request.get_json()
-    reviews.append(data)  # Сохраняем отзыв
-    return jsonify({"message": "Отзыв отправлен!"}), 201
+    try:
+        data = request.get_json()
+        if 'name' not in data or 'review' not in data or 'ownership' not in data:
+            return jsonify({'error': 'Недостаточно данных'}), 400
+        reviews.append(data)
+        return jsonify({"message": "Отзыв отправлен!"}), 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 @app.route('/api/reviews', methods=['GET'])
 def get_reviews():
